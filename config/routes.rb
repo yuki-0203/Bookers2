e98cd2ad7c6dbb7d91c6a76ae4a39ba0404a02b2
EC2 Default User <ec2-user@ip-172-31-18-144.ec2.internal>
2021-12-10 13:20:18 +0000

    User

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  get 'users/show'
+  devise_for :users, controllers: { registrations: 'users/registrations' }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'homes#top'
+  get "users/show" => "users#show"
 end
