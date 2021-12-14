55a3fea34fa652251fb01b046a921813eb544e39
EC2 Default User <ec2-user@ip-172-31-18-144.ec2.internal>
2021-12-14 02:33:15 +0000

    book complete

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -6,26 +6,56 @@ class BooksController < ApplicationController
 
   def show
      @book = Book.new
-    @books = Book.all.order(created_at: :desc)
+     @book_id=Book.find(params[:id])
   end
 
   def new
     @book = Book.new
   end
   
+
   def create
     @book = Book.new(book_params)
     @book.user_id=current_user.id
-    @book.save
-     redirect_to books_path
+    if @book.save
+        flash[:notice] = "Book was successfully created." 
+        redirect_to book_path(@book)
+    else
+      render "index"
+    end
   end
 
   def edit
+    @book=Book.find(params[:id])
   end
+  
+  def update
+   @book=Book.find(params[:id])
+    if @book.user == current_user
+      if @book.update(book_params)
+          flash[:notice] = "Book was successfully updated." 
+          redirect_to book_path(@book)
+      else
+          render "edit"
+      end
+    end
+  end
+  
+  
+  
+  def destroy
+  @book=Book.find(params[:id])
+  if @book.user == current_user
+   @book.destroy 
+  end
+  redirect_to books_path
+  end
+  
   
     private
 
   def book_params
     params.require(:book).permit(:title,:body,:user)
   end
+  
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,11 +2,15 @@ class UsersController < ApplicationController
  def index
     @users = User.all
     @user = current_user
+    @book = Book.new
  end
  
   def show
     @users = User.all
     @user = User.find(params[:id])
+    @book = Book.new
+    @books = Book.where(user: @user)
+
   end
   
   def edit
@@ -21,6 +25,13 @@ class UsersController < ApplicationController
   
   def new
    @book = Book.new
+  end
+  
+  def create
+    @book = Book.new(book_params)
+    @book.user_id=current_user.id
+    @book.save
+    redirect_to books_path
   end
   
 private
